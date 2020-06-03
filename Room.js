5ba2cdd6df3d140d3097bff2c20ac5b771fe1127
enjoipotter <enjoipotter@gmail.com>
2020-06-03 12:36:21 -0700

    added docs and refactored

--- Room.js
+++ Room.js
@@ -1,7 +1,15 @@
-
-module.exports = class Room {
+/**
+ * @class Room represents a chat room.
+ */
+class Room {
+  /**
+   * Creates a new room.
+   * @param {string} roomName - The name of the room to create.
+   */
   constructor(roomName) {
     this.name = roomName;
     this.members = [];
   }
-};
+}
+
+module.exports = Room;
--- User.js
+++ User.js
@@ -1,8 +1,13 @@
-module.exports = class User {
+/**
+ * @class User represents a chat user.
+ */
+class User {
   constructor(username, hostname, realname, socket) {
     this.username = username;
     this.hostname = hostname;
     this.realname = realname;
     this.socket = socket;
   }
-};
+}
+
+module.exports = User;
